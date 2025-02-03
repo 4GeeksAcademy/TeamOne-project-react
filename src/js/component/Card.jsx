c28af0372adcdae95e67f48657a986e09de549fc
Ángel Sánchez Moreno <univ.angel97@gmail.com>
2025-02-03 12:22:19 +0000

    problema solucionado de las cards

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
     return (
-        <div className="card" style="width: 18rem;">
+        <div>
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
