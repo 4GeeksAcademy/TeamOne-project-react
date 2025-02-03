ea980f062066861725789de055d9fff65a36150d
Ángel Sánchez Moreno <univ.angel97@gmail.com>
2025-02-03 11:57:21 +0000

    Jumbotron final hecho por Angel Sanchez

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -6,7 +6,7 @@ const Jumbotron = () => {
             <div className="container">
                 <div className=" container-fluid py-5 bg-secondary rounded">
                     <h1 className="display-5 fw-bold"> A warm welcome! </h1>
-                    <p className="col-md-8 fs-4 m-l-auto">
+                    <p className="col-md-8 fs-4 m-l-auto" style={{"text-align":"justify"}}>
                         Lorem ipsum dolor sit amet, consectetur adispisicing elit, ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid smilique quaerat  narm nobis ilo aspernatur vitae fugia numquam repletiat
                     </p>
                     <button class="btn btn-primary btn-lg" type="button">Example button</button>
